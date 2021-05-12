a0b398057a85171caaf052dac11c6fe2da0c5fdd
tommaso <scocciolinitommaso@gmail.com>
2021-05-12 18:23:10 +0200

    base layout scss

--- resources/views/partials/header.blade.php
+++ resources/views/partials/header.blade.php
@@ -1,6 +1,31 @@
 <header>
   <div class="container">
-
+    <div class="container-top-bar">
+      <p>DC POWER VISA R</p>
+      <p>ADDICTIONAL DC SITES</p>
+    </div>
+  </div>
+  <div class="container-big">
+    <div class="container-nav">
+      <ul>
+        <li> <img src="img/dc-logo.png" alt=""> </li>
+        <li class="text">CHARACTERS</li>
+        <li class="text active">COMICS</li>
+        <li class="text">MOVIES</li>
+        <li class="text">TV</li>
+        <li class="text">GAMES</li>
+        <li class="text">COLLECTIBLES</li>
+        <li class="text">VIDEOS</li>
+        <li class="text">FANS</li>
+        <li class="text">NEWS</li>
+        <li class="text">SHOP</li>
+        <li>
+          <input type="text" name="search" value="" placeholder="Search">
+        </li>
+      </ul>
+    </div>
+  </div>
+  <div class="jumbo" style="background-image: url('/img/jumbotron.jpg');">
 
   </div>
 </header>
